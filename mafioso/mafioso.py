546c2311ebd8715d95627c64150d6cf3c7600c24
Ethan <ethan@max90.co.uk>
2021-06-11 18:12:32 +0100

    no

--- mafioso/mafioso.py
+++ mafioso/mafioso.py
@@ -94,7 +94,7 @@ class Mafioso(commands.Cog):
 
         to_print = f'**Signed up** | {self.nosu}\n' #title for the list
 
-        to_print += '\n'.join(f'{member.mention}  ({member.display_name})  {emoji}' for member, emoji in self.players.values())
+        to_print += '\n'.join(f'{member.display_name}  ({member.mention})  {emoji}' for member, emoji in self.players.values())
         message = await ctx.send('.')
         await message.edit(content=to_print)
         #lists all signed up players in players list
