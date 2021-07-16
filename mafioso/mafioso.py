6526ea167c54b6b37d94d3d501a3b0c47e2eff70
Ethan <ethan@max90.co.uk>
2021-07-16 20:48:41 +0100

    update mafioso.py

--- mafioso/mafioso.py
+++ mafioso/mafioso.py
@@ -45,6 +45,10 @@ class Mafioso(commands.Cog):
         self.backup_role = 797377006659436576
         self.alive_role = 796806682344161320
         self.dead_role = 796806821490589796
+        self.moderator_role = 856971152978608188
+        self.admin_role = 796806958329495624
+        self.mod_ingame_role = 847850195366707310
+        self.admin_ingame_role = 796808073129623553
         self.config = Config.get_conf(self, identifier=6, force_registration=True)
         self.default_player = {"obj": None, "emoji": 'None', "alive": False, "role": None}
 
@@ -75,7 +79,7 @@ class Mafioso(commands.Cog):
             member = guild.get_member(member_id)
             if member is not None:  # None is failed to get the user.
                 # (Quit the server, programming error)
-                self.players[member_id] = {"obj": member, "emoji": 'emoji', "alive": alive, "role": role}
+                self.players[member_id] = {"obj": member, "emoji": emoji, "alive": alive, "role": role}
             else:
                 log.warning(f"Failed to get id: {member_id}")
 
@@ -86,15 +90,17 @@ class Mafioso(commands.Cog):
         for member_id, player_dict in self.players.items():
             member = player_dict["obj"]
             emoji = player_dict["emoji"]
+            log.info(f"{type(emoji)}")
             guild_id = member.guild.id  # Fetch the guild ID for this batch of members
-            if emoji is discord.Emoji:  # Check if the emoji is a custom emoji
+            if emoji is discord.Emoji or emoji is discord.emoji.Emoji:
+                log.info(f"{emoji} is discord.Emoji")
                 emoji = emoji.id  # Save the custom emoji ID
 
             # This is the structure of the config file now. Only emoji (as id), alive status, and role (tbd)
             this_player = {"emoji": emoji, "alive": player_dict["alive"], "role": player_dict["role"]}
 
             saveable_players[member.id] = this_player
-
+        log.info(saveable_players)
         await self.config.players.set(saveable_players)
         await self.config.guild_id.set(guild_id)  # Save the guild_id
 
@@ -108,7 +114,7 @@ class Mafioso(commands.Cog):
     @commands.command()
     async def signup(self, ctx: commands.Context, emoji: RealEmojiConverter):
         if not self.check_for_duplicates(ctx.author, emoji):
-            await ctx.send("Member or emote invalid")
+            await ctx.send(":x: Duplicate emoji or player found")
             return
 
         player_dict = self.default_player.copy()
@@ -219,7 +225,6 @@ class Mafioso(commands.Cog):
             member = player_dict['obj']
             role = ctx.guild.get_role(self.signed_up_role)
             alive = ctx.guild.get_role(self.alive_role)
-            self.nosu = self.nosu - 1
             await member.remove_roles(role, reason="starting game")
             await member.add_roles(alive, reason="starting game")
             player_dict['alive'] = True
@@ -265,3 +270,37 @@ class Mafioso(commands.Cog):
 
         await ctx.send(f'{sheet_list}')
         # lists all signed up players in players list
+
+    @commands.command()
+    async def demote(self, ctx: commands.Context):
+        mod = ctx.guild.get_role(self.moderator_role)
+        mod_ig = ctx.guild.get_role(self.mod_ingame_role)
+        admin = ctx.guild.get_role(self.admin_role)
+        admin_ig = ctx.guild.get_role(self.admin_ingame_role)
+        if "856971152978608188" in [role.id for role in ctx.message.author.roles]:
+            await member.remove_roles(mod, reason="demote")
+            await member.add_roles(mod_ig, reason="demote")
+            await ctx.send("Demoted from moderator")
+        if "796806958329495624" in [role.id for role in ctx.message.author.roles]:
+            await member.remove_roles(admin, reason="demote")
+            await member.add_roles(admin_ig, reason="demote")
+            await ctx.send("Demoted from admin")
+        else:
+            await ctx.send(":x: you do not have the permissions for that")
+
+    @commands.command()
+    async def promote(self, ctx: commands.Context):
+        mod = ctx.guild.get_role(self.moderator_role)
+        mod_ig = ctx.guild.get_role(self.mod_ingame_role)
+        admin = ctx.guild.get_role(self.admin_role)
+        admin_ig = ctx.guild.get_role(self.admin_ingame_role)
+        if "856971152978608188" in [role.id for role in ctx.message.author.roles]:
+            await member.remove_roles(mod_ig, reason="promote")
+            await member.add_roles(mod, reason="promote")
+            await ctx.send("Promoted to moderator")
+        if "796806958329495624" in [role.id for role in ctx.message.author.roles]:
+            await member.remove_roles(admin_ig, reason="promote")
+            await member.add_roles(admin, reason="promote")
+            await ctx.send("Promoted to admin")
+        else:
+            await ctx.send(":x: you do not have the permissions for that")
