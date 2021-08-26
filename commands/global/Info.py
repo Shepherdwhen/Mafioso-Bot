a974ea83bb4b6d7c45e08f5347cc0c5b532ca46a
abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2021-08-26 10:58:23 +0200

    Add info command

---
+++ commands/global/Info.py
@@ -0,0 +1,30 @@
+from discord.ext import commands
+
+from mafia.util import RoleConverter
+
+TEMPLATE = """
+**{name}** - {alignment} {power}
+
+{description}
+"""
+
+class Info(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(
+        name='info',
+        aliases=[
+            'i'
+        ]
+    )
+    async def info(self, ctx, role: RoleConverter):
+        description_str = '\n'.join(role['description'])
+
+        await ctx.send(TEMPLATE.format(
+            name=role['name'],
+            alignment=role['alignment'],
+            power=role['power'],
+            description=description_str
+        ))
--- commands/global/__init__.py
+++ commands/global/__init__.py
@@ -1,5 +1,7 @@
+from .Info import Info
 from .Ping import Ping
 
 
 def setup(bot):
     bot.add_cog(Ping(bot))
+    bot.add_cog(Info(bot))
