28f6a150c487d3aedecb65b7480226cc00fb9c44
abitofevrything <54505189+abitofevrything@users.noreply.github.com>
2021-08-28 15:12:48 +0200

    Show emoji and nickname in list commands

--- commands/ingame/List.py
+++ commands/ingame/List.py
@@ -29,20 +29,30 @@ class List(commands.Cog):
     async def list(self, ctx):
         with sqlite3.connect('database.sqlite3') as connection:
             data = connection.execute("""
-            SELECT user_id, nick FROM player_data
+            SELECT user_id, nick, emoji FROM player_data
             """).fetchall()
 
         id_to_nick = {row[0]: row[1] for row in data if row[1]}
+        id_to_emoji = {row[0]: row[2] for row in data if row[2]}
 
 
         hosts = globvars.state_manager.game.hosts
-        hosts_str = ' - ' + ('\n - '.join((id_to_nick[p.id] if p.id in id_to_nick else p.display_name) for p in hosts) if hosts else 'No host')
+        hosts_str = ' - ' + ('\n - '.join(
+            ((id_to_nick[p.id] if p.id in id_to_nick else p.display_name) + (' (' + id_to_emoji[p.id] + ')' if p.id in id_to_emoji else '')) \
+            for p in hosts \
+        ) if hosts else 'No host')
 
         alive_players = globvars.state_manager.game.alive_players
-        alive_player_str = ' - ' + ('\n - '.join((id_to_nick[p.id] if p.id in id_to_nick else p.display_name) for p in alive_players) if alive_players else 'No players')
+        alive_player_str = ' - ' + ('\n - '.join(
+            ((id_to_nick[p.id] if p.id in id_to_nick else p.display_name) + (' (' + id_to_emoji[p.id] + ')' if p.id in id_to_emoji else '')) \
+            for p in alive_players \
+        ) if alive_players else 'No players')
         
         dead_players = globvars.state_manager.game.dead_players
-        dead_player_str = ' - ' + ('\n - '.join((id_to_nick[p.id] if p.id in id_to_nick else p.display_name) for p in dead_players) if dead_players else 'No players')
+        dead_player_str = ' - ' + ('\n - '.join(
+            ((id_to_nick[p.id] if p.id in id_to_nick else p.display_name) + (' (' + id_to_emoji[p.id] + ')' if p.id in id_to_emoji else '')) \
+            for p in dead_players \
+        ) if dead_players else 'No players')
 
         await ctx.send(TEMPLATE.format(
             hosts=hosts_str,
--- commands/pregame/List.py
+++ commands/pregame/List.py
@@ -26,17 +26,24 @@ class List(commands.Cog):
     async def list(self, ctx):
         with sqlite3.connect('database.sqlite3') as connection:
             data = connection.execute("""
-            SELECT user_id, nick FROM player_data
+            SELECT user_id, nick, emoji FROM player_data
             """).fetchall()
 
         id_to_nick = {row[0]: row[1] for row in data if row[1]}
+        id_to_emoji = {row[0]: row[2] for row in data if row[2]}
 
 
         hosts = globvars.state_manager.pregame.hosts
-        hosts_str = ' - ' + ('\n - '.join((id_to_nick[p.id] if p.id in id_to_nick else p.display_name) for p in hosts) if hosts else 'No host')
+        hosts_str = ' - ' + ('\n - '.join(
+            ((id_to_nick[p.id] if p.id in id_to_nick else p.display_name) + (' (' + id_to_emoji[p.id] + ')' if p.id in id_to_emoji else '')) \
+            for p in hosts \
+        ) if hosts else 'No host')
 
         players = globvars.state_manager.pregame.queue
-        player_str = ' - ' + ('\n - '.join((id_to_nick[p.id] if p.id in id_to_nick else p.display_name) for p in players) if players else 'No players')
+        player_str = ' - ' + ('\n - '.join(
+            ((id_to_nick[p.id] if p.id in id_to_nick else p.display_name) + (' (' + id_to_emoji[p.id] + ')' if p.id in id_to_emoji else '')) \
+            for p in players \
+        ) if players else 'No players')
 
         await ctx.send(TEMPLATE.format(
             hosts=hosts_str,
